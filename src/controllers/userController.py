d3b5ac171809baca1d85a47dce0c19cc78e49758
Ahmed KACEM <a.kacem@gexpertise.fr>
2025-02-04 15:15:33 +0100

    add : next commit will change the slightly the project structure and code

--- src/controllers/userController.py
+++ src/controllers/userController.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import Blueprint
 
+blueprint = Blueprint('user',__name__, url_prefix= '/bye')
 app = Flask(__name__)
-@app.route("/user")
+@blueprint.route("/user")
