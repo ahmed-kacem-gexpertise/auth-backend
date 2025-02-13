432eed494987ec825bacef2931611c3e53aa557c
Ahmed KACEM <a.kacem@gexpertise.fr>
2025-02-13 15:38:52 +0100

    add : rememberMe attribute added to the backend to extend the expiry time of the tokens if true

--- app/api/api_schemas.py
+++ app/api/api_schemas.py
@@ -8,6 +8,7 @@ class LoginUserSchema(Schema):
         error_messages={"invalid": "Please provide a valid email address"}
     )
     password = fields.String(required=True)
+    rememberMe = fields.Boolean(default=False, description="Remember Me option")
 class RegisterUserSchema(Schema):
     email = fields.Email(
         required=True,
--- app/api/services/auth_service.py
+++ app/api/services/auth_service.py
@@ -1,18 +1,25 @@
 from ...models.user_model import User
 from flask import jsonify,make_response
+from datetime import timedelta
+
 from flask_jwt_extended import (
    create_access_token,
    create_refresh_token,
     get_jwt_identity, set_access_cookies,
     set_refresh_cookies, unset_jwt_cookies
 )
-def authenticate(email, password):
+def authenticate(email, password,rememberMe):
     user = User.query.filter_by(email=email).first()
-    
+    if rememberMe:
+        access_expires_in = timedelta(days=30)  
+        refresh_expires_in = timedelta(days=30) 
+    else:
+        access_expires_in = timedelta(minutes=15) 
+        refresh_expires_in = timedelta(days=7)  
     if user and user.verify_password(password):  
          
-        access_token = create_access_token(identity=str(user.id))
-        refresh_token   = create_refresh_token(identity=str(user.id))
+        access_token = create_access_token(identity=str(user.id),expires_delta=access_expires_in)
+        refresh_token   = create_refresh_token(identity=str(user.id),expires_delta=refresh_expires_in)
         
       
         response_data = {
